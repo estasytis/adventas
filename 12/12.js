fec753fa03622a98fc44a020eb2728a4a2401eb5
estasytis <estasytis@gmail.com>
2023-12-12 15:19:27 +0200

    12 diena

---
+++ 12/12.js
@@ -0,0 +1,19 @@
+
+let height=window.prompt("Enter the height of the tree");
+tree(height);
+
+function tree(height)
+{
+   let layer="*";
+   let part=height;
+   for(let i=0; i<height; i++)
+   {
+      let p1=" ";
+      p1=p1.repeat(part);
+      part--;
+
+      console.log(p1+layer); 
+      layer=layer+"**";
+   }
+
+}
